919e3a96c611328c3b93c6cf263c125abc0d96f1
atellezm1 <atellezm@unjbg.edu.pe>
2024-06-30 22:33:55 -0500

    Creacoción de un proyecto en Dev C++ y archivo de Cabecera

---
+++ funciones/main.cpp
@@ -0,0 +1,11 @@
+#include<iostram>
+#include "funciones.h"
+
+using namespace std;
+
+int main(){
+	in a = 10, b= 5;
+	cout<< "Suma: "<< suma(a,b)<<endl;
+	cout<< "Resta "<< resta(a,b)<<endl;
+	return 0;
+}
