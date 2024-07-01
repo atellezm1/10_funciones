0669e6c3cfb349d50fc009eea7c1c2ebbefe2b22
atellezm1 <atellezm@unjbg.edu.pe>
2024-06-30 20:28:14 -0500

    Inmplementación de las Funciones y Uso de las Variables Gloval

--- funciones/ejercicio1.cpp
+++ funciones/ejercicio1.cpp
@@ -1,27 +1,30 @@
 //Allexander Téllez Mesa
 #include<iostream>
 using namespace std;
+void depositar();
+void retirar();
+void mostrarSaldo();
 void menu(){
 	int opc;
     do {
         cout<<"\n--- Cajero automatico ---"<<endl;
         cout<<"1. depositar 100 "<<endl;
-        cout<<"2. Retirar 1oo "<<endl;
-        cout<<"3. Mostrar ssaldo "<<endl;
-        cout<<"4. Sali "<<endl;
-        cout<<"Seleccione una opción: ";cin>>opc;
+        cout<<"2. Retirar 100 "<<endl;
+        cout<<"3. Mostrar saldo "<<endl;
+        cout<<"4. Salir "<<endl;
+        cout<<"Seleccione una opcion: ";cin>>opc;
 
         switch(opc){
             case 1:{
-
+                    depositar();
                 break;
             }
             case 2:{
-
+                    retirar();
                 break;
             }
             case 3:{
-
+                   mostrarSaldo(); 
                 break;
             }
             case 4:{
@@ -31,11 +34,29 @@ void menu(){
             default:{
                 cout<<"Opcion no valida.Intente de nuevo."<<endl;
             }
-        }
-
-
+        }    
     }while(opc !=4);   
-return 0;
+}
+
+int saldo = 0;
+
+void depositar(){
+    saldo+=100;
+    cout<<"Se a depositado 100 unidades. Nuevo saldo: "<<saldo<<endl;
+}
+
+void retirar(){
+    if(saldo<=100){
+        saldo-=100;
+        cout<<"Se han retirado 100 unidades. Nuevo saldo: "<<saldo<<endl;
+    }
+    else{
+        cout<<"Saldo insuficiente."<<endl;
+    }
+}
+
+void mostrarSaldo(){
+    cout<<"Saldo actual: "<<saldo<<endl;
 }
 
 int main(){
